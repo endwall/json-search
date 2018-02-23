412cb53b8fe3032b460f9412d34aa0bc65c8d88d
Yongjun Deng <yongjun.deng@sportsbet.com.au>
2018-02-23 23:15:17 +1100

    update test case

--- client/tests/test_json_search.py
+++ client/tests/test_json_search.py
@@ -1,4 +1,4 @@
-"""Unit testing for the common.custom.stacker_commands module."""
+"""Unit testing for the module."""
 import argparse
 from unittest import TestCase
 
@@ -6,7 +6,7 @@ from mock import MagicMock, call, patch
 from nose.tools import raises
 
 class TestJsonSearch(TestCase):
-    """Test the commands module."""
+    """Test the module."""
 
     def setUp(self):
         """Do setup."""
@@ -26,25 +26,4 @@ class TestJsonSearch(TestCase):
         self.options.region = 'ap-southeast-2'
         self.options.environment = {}
 
-    @patch('common.custom.stacker_commands.parse_environment')
-    @patch('common.custom.stacker_commands.open')
-    @patch('common.custom.stacker_commands.os.path.isfile')
-    @patch('common.custom.stacker_commands.sys')
     def test_get_common_environment(self, mock_sys, mock_isfile, mock_open, mock_parse_environment):
-        """Get common environment values."""
-        mock_sys.argv = ['foo/bar/dev.env']
-        mock_isfile.return_value = True
-        mock_open.return_value = MagicMock(spec=file)
-        mock_parse_environment.return_value = {'foo': 'bar'}
-
-        result = stacker_commands.get_common_environment(self.options.environment)
-
-        self.assertTrue(mock_isfile.called)
-        self.assertEqual(mock_isfile.call_args, call('foo/bar/common.env'))
-        self.assertTrue(mock_open.called)
-        self.assertEqual(mock_open.call_args, call('foo/bar/common.env', 'r'))
-        self.assertTrue(mock_parse_environment.called)
-        self.assertEqual(result, {
-            'foo': 'bar',
-            'env': 'dev'
-        })
